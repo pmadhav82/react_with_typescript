394da80867133d8d9d28f105935d641c921b9141
Madhav Pandey <pmadhav279@gmail.com>
2024-09-24 11:52:16 +1000

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+ 
+
+  return (
+    <> <div className="container">
+
+<h1>Welcome to React with Typescript course</h1>
+<button className="btn btn-primary">Get Started</button>
+
+    </div>
+    </>
+  )
+}
+
+export default App
